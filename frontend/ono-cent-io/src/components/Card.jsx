f2ac041b80ae606ed2c75e879724727d42334d79
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-06-01 19:50:27 -0300

    Ajustes No Front

--- frontend/ono-cent-io/src/components/Card.jsx
+++ frontend/ono-cent-io/src/components/Card.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-const Card = ({ title, image, price }) => {
+import client from '../util/socketClient';
+const Card = ({ id, name, image, price }) => {
+  const [currentPrice, setCurrentPrice] = useState(price);
+  useEffect(() => {
+    client.on('updatePriceClient', (product) => {
+      if (product._id === id) setCurrentPrice(product.price);
+    });
+  }, [id]);
+
+  const handleClick = () => {
+    client.emit('updatePrice', { id });
+  }
+
+
   return (
     <div className="card-body">
-      <div className="card-title">{title}</div>
+      <div className="card-title">{name}</div>
       <div className="card-image">
         <img src={image} alt="product" className="image-product" />
       </div>
-      <div className="card-price">{price}</div>
+      <div className="card-price">{currentPrice}</div>
       <div className="card-button">
-        <button type="button" className="btn-bid">
-          Dar lance
-        </button>
+        {currentPrice < 100 
+        ? (<button type="button" className="btn-bid" onClick={handleClick}>
+            Dar lance
+          </button>) :
+          (<button type="button" className="btn-bid">
+          Produto Arrematado
+        </button>)}
+        
       </div>
     </div>
   );
--- frontend/ono-cent-io/src/pages/home.js
+++ frontend/ono-cent-io/src/pages/home.js
@@ -2,8 +2,10 @@ import React, {useEffect, useState} from 'react'
 import Header from '../components/header';
 import Card from '../components/Card';
 
+// import client from '../util/socketClient';
+
 const Home = () => {
-  const [cardData, setCardData] = useState({name:'test', image:'', price:'0,00'});
+  const [cardData, setCardData] = useState([{name:'test', image:'', price:'0,00'}]);
 
   
   useEffect(() => {
@@ -20,11 +22,16 @@ const Home = () => {
             <Header />
           </div>
           <div className="container-card">
-            <Card 
-            title={cardData.title}
-            image={cardData.image}
-            price={cardData.price}
-            />
+            {cardData && cardData.map((product => (
+              <Card 
+                key={product._id}
+                id={product._id}
+                name={product.name}
+                image={product.image}
+                price={product.price}
+              />
+            ))) }
+            
           </div>
       </div>
     )
